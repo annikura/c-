d6413abb184f20646dbf08074fbd3159e30361f9
annikura <annikura@yandex.ru>
2016-09-22 20:29:31 +0300

    tests_str.h was removed for the include directory

--- lab_02/include/tests_str.h
+++
@@ -1,7 +0,0 @@
-#ifndef __TESTS_STR
-    #define __TESTS_STR
-    void test_strlen(int n);
-    void test_strcat(int n);
-    void test_strcpy(int n);
-    void test_strcmp(int n);
-#endif // __TESTS_STR
